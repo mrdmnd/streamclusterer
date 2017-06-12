aa4c398d2230161ed35dfe7512071e751a7ecf59
Matt Redmond <mredmond@strava.com>
2017-06-12 00:22:59 -0700

    Structural commit.

---
+++ tests/vendor/hello_unit.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "test.h"
+
+void hello_test() {
+  assert_true(32 == 32);
+}
+
+int main() {
+  const struct CMUnitTest tests[] = {
+    cmocka_unit_test(hello_test)
+  };
+
+  return cmocka_run_group_tests(tests, NULL, NULL);
+}
---
+++ tools/cmake/FindCTargets.cmake
@@ -0,0 +1,16 @@
+if (CMAKE_VERSION VERSION_GREATER 3.0.0)
+    cmake_policy(VERSION 3.0.0)
+endif ()
+
+function(add_c_test TEST_NAME)
+    #set_source_files_properties(${TEST_NAME}.c COMPILE_FLAGS "-ggdb -O1")
+    add_executable(${TEST_NAME} ${TEST_NAME}.c)
+    target_link_libraries(${TEST_NAME} ${STREAMGEO_LIB_NAME} cmocka m)
+    add_test(${TEST_NAME} ${TEST_NAME})
+endfunction(add_c_test)
+
+function(add_c_benchmark BENCH_NAME)
+    #set_source_files_properties(${BENCH_NAME}.c COMPILE_FLAGS "-O3")
+    add_executable(${BENCH_NAME} ${BENCH_NAME}.c)
+    target_link_libraries(${BENCH_NAME} ${STREAMGEO_LIB_NAME} m)
+endfunction(add_c_benchmark)
